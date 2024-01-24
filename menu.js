8555ab50e6690ad24e1e7f507f513aea6dee7ead
Erik Lisboa <eriklisboa72@outlook.com>
2024-01-23 23:46:41 -0300

    first commit

---
+++ menu.js
@@ -0,0 +1,26 @@
+function Menu() {
+    this.windowWidth = window.innerWidth;
+    this.OpenMenu   = OpenMenu;
+    this.CloseMenu  = CloseMenu;
+
+    function OpenMenu(_id) {
+        if (this.windowWidth <= 900) {
+            document.getElementById(_id).style.width = "100%";
+        } 
+    }
+
+    function CloseMenu(_id) {
+        if (this.windowWidth <= 900) {
+            document.getElementById(_id).style.width = "0%";
+        }
+    }
+}
+
+var navbar = new Menu();
+function openNavbar() {
+        navbar.OpenMenu("content-menu");
+}
+function closeNavbar() {
+    navbar.CloseMenu("content-menu");
+}
+
